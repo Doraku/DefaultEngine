27b6f21892a64f5d6e0a99126eb989d21cdbb342
Laszlo Paillat <laszlo.paillat@gmail.com>
2025-08-17 15:19:05 +0200

    fixe weird contentpresenter content binding issue

--- Avalonia.DefaultLayout/Internal/Views/StackedLayoutContentView.axaml.cs
+++ Avalonia.DefaultLayout/Internal/Views/StackedLayoutContentView.axaml.cs
@@ -1,9 +1,12 @@
+using System;
 using Avalonia.Controls;
 
 namespace Avalonia.DefaultLayout.Internal.Views;
 
-public sealed partial class StackedLayoutContentView : UserControl
+public sealed partial class StackedLayoutContentView : TabControl
 {
+    protected override Type StyleKeyOverride => typeof(TabControl);
+
     public StackedLayoutContentView()
     {
         InitializeComponent();
